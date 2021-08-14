7b9b34c8f7e4476094a75fb9f68d025c3284ccca
tzhu <tzhu@corp.amazonworkspaces.com>
2021-08-13 23:36:07 -0400

    add pre_release.bat

--- lambda_function.py
+++ lambda_function.py
@@ -1,8 +1,30 @@
+import os
 import json
+import boto3
+
+
+# get env
+env = os.environ.get("env")
+# boto3 S3 initialization
+s3_client = boto3.client("s3")
+
 
 def lambda_handler(event, context):
-    # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Tingting!')
-    }
+   destination_bucket_name = 'outbound-data-bucket'
+
+   # Bucket Name where file was uploaded
+   source_bucket_name = event['Records'][0]['s3']['bucket']['name']
+   source_file_key_name = event['Records'][0]['s3']['object']['key']
+   # Filename of object (with path)
+   desitination_file_key_name = f'{env}/' + source_file_key_name
+
+   # Copy Source Object
+   copy_source_object = {'Bucket': source_bucket_name, 'Key': source_file_key_name}
+
+   # S3 copy object operation
+   s3_client.copy_object(CopySource=copy_source_object, Bucket=destination_bucket_name, Key=desitination_file_key_name)
+
+   return {
+       'statusCode': 200,
+       'body': json.dumps('Process s3 data Job Done!')
+   }
