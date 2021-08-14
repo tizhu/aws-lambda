8f49601f4d0cc67d6081ec5f0306c1691dfb2998
tzhu <tzhu@corp.amazonworkspaces.com>
2021-08-13 23:36:57 -0400

    another change

--- pre_release.bat
+++ pre_release.bat
@@ -1,4 +1,4 @@
 zip -j deploy.zip lambda_function.py
 aws lambda update-function-code --function-name process_s3_data_function --zip-file=fileb://deploy.zip
 aws lambda update-function-configuration --function-name process_s3_data_function  --environment 'Variables={env=dev}'
-aws lambda invoke --function-name process_s3_data_function --invocation-type Event --payload fileb://events/inputFile.txt
+aws lambda invoke --function-name process_s3_data_function --invocation-type RequestResponse --payload fileb://events/inputFile.txt response_output.json
